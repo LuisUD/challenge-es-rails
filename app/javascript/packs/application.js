f381b1657c3d5268550a962041ac567088f52ddf
LuisUD <llemt_53@hotmail.com>
2020-09-27 16:50:24 -0500

    font

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
-import '@fortawesome/fontawesome-free';
-import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all";
 import 'controllers'
 
 
